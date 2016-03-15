b49c0cacaa8629aa204e5e8773bcc7e87c985b09
Matthijs <matthijsthoolen@hotmail.com>
2016-03-15 16:12:55 +0100

    Styling and more

--- Week 2/server.py
+++ Week 2/server.py
@@ -1,6 +1,7 @@
 from tornado import httpserver
 from tornado import gen
 from tornado.ioloop import IOLoop
+from tornado import template
 import sqlite3 as sqlite
 import tornado.web
 
@@ -8,7 +9,7 @@ class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.write('Hello, world!')
 
-class CarHandler(tornado.web.RequestHandler):
+class StoreHandler(tornado.web.RequestHandler):
     def get(self):
         self.write('GET - Welcome to our document store!')
 
@@ -22,6 +23,20 @@ class CarHandler(tornado.web.RequestHandler):
         self.clear()
         self.set_status(501)
         self.finish("<html><body>Method not Implemented</body></html>")
+
+class ApiInterface(tornado.web.RequestHandler):
+    def get(self):
+
+        loader = template.Loader("templates")
+        self.write(loader.load("index.html").generate(myvalue="Wow this is awesome VIA", alert=None, current_user="anonymous"))
+
+        #t = template.Template("<html>{{ myvalue }}</html>")
+        #self.write(t.generate(myvalue="XXX"))
+
+    def post(self):
+        self.clear()
+        self.set_status(405)
+        self.finish("<html><body>POST not supported!</body></html>")
 
 def verifyDatabase():
     conn = sqlite.connect('cars.db')
@@ -45,9 +60,10 @@ def verifyDatabase():
 class Application(tornado.web.Application):
     def __init__(self):
         handlers = [
-            (r"/?", MainHandler),
-            (r"/api/v1/document/?", CarHandler),
-            (r"/api/v1/document/[0-9][0-9][0-9][0-9]/?", CarHandler)
+            (r"/?", ApiInterface),
+            (r"/api/v1/document/?", StoreHandler),
+            (r'/static/(.*)', tornado.web.StaticFileHandler, {'path': 'static'}),
+            (r"/api/v1/document/[0-9][0-9][0-9][0-9]/?", StoreHandler)
         ]
         tornado.web.Application.__init__(self, handlers)
 
