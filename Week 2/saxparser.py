77b419e7c39596d240c220ac4247d216c55a8df5
xanderkoning <xander.koning@student.uva.nl>
2016-03-17 13:12:42 +0100

    saxparser begin

---
+++ Week 2/saxparser.py
@@ -0,0 +1,47 @@
+import xml.sax
+from yamr import Database, Chunk, Tree
+from server import *
+
+
+class NVDHandler(xml.sax.ContentHandler):
+
+    def __init__(self):
+        self.CurrentData = ""
+        self.db = getDb('nvd.db')
+        self.id = ""
+        self.products = []
+
+    # Call when an element starts
+    def startElement(self, tag, attributes):
+        self.CurrentData = tag
+        if tag == "entry":
+            print("*****Entry*****")
+            self.id = attributes["id"]
+            print("ID:", self.id)
+
+    # Call when an elements ends
+    def endElement(self, tag):
+        if self.CurrentData == "entry":
+            self.db[self.id] = self.products
+            self.products = []
+        elif self.CurrentData == "nvd":
+            self.db.commit()
+            CloseDb(self.db)
+
+        self.CurrentData = ""
+
+    # Call when a character is read
+    def characters(self, content):
+        if self.CurrentData == "vuln:product":
+            self.products += [content]
+
+if (__name__ == "__main__"):
+    # create an XMLReader
+    parser = xml.sax.make_parser()
+    # turn off namepsaces
+    parser.setFeature(xml.sax.handler.feature_namespaces, 0)
+
+    # override the default ContextHandler
+    Handler = NVDHandler()
+    parser.setContentHandler(Handler)
+    parser.parse("Database files/test.xml")
